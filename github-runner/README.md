e939d53611070b3b8989fdd07d1f15adc6601f59
Moim HA <moimhossain@gmail.com>
2022-11-01 15:28:04 +0100

    Docker image for GH Runner

---
+++ github-runner/README.md
@@ -0,0 +1,7 @@
+
+
+GHUSER
+
+```
+docker run --rm -e PAT='' -e GHUSER='moimhossain' -e REPO='github-runner-azure-container-app-hosted' moimhossain/ghrunner:beta
+```
---
+++ github-runner/spin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+GHUSER=$GHUSER
+REPO=$REPO
+PAT=$PAT
+
+RUNNER_NAME="RUNNER-$(hostname)"
+
+TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${PAT}" https://api.github.com/repos/${GHUSER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+cd /home/docker/actions-runner
+./config.sh --unattended --url https://github.com/${GHUSER}/${REPO} --token ${TOKEN} --name ${RUNNER_NAME} --ephemeral
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
