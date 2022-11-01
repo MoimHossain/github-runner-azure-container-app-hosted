15abac55341517994ed645cfb917386c5011d950
Moim HA <moimhossain@gmail.com>
2022-11-01 15:47:50 +0100

    Disable updates

--- github-runner/spin.sh
+++ github-runner/spin.sh
@@ -7,8 +7,7 @@ RUNNER_NAME="RUNNER-$(hostname)"
 
 TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${PAT}" https://api.github.com/repos/${GHUSER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 cd /home/docker/actions-runner
-./config.sh --unattended --url https://github.com/${GHUSER}/${REPO} --token ${TOKEN} --name ${RUNNER_NAME} --ephemeral 
-# --disableupdate
+./config.sh --unattended --url https://github.com/${GHUSER}/${REPO} --token ${TOKEN} --name ${RUNNER_NAME} --ephemeral --disableupdate
 
 cleanup() {
     echo "Removing runner..."
